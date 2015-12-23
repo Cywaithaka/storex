aa6126aca03fd5fa6179b7ec39ed32468fa6c69d
MrDonald245 <ebocharnikov96@mail.ru>
2015-12-23 21:36:52 +0200

    Добавление js и css скриптов
    Возможно это не все скрипты.

--- wp-content/themes/storex/functions.php
+++ wp-content/themes/storex/functions.php
@@ -115,10 +115,15 @@ add_action( 'widgets_init', 'storex_widgets_init' );
  */
 function storex_scripts() {
 	wp_enqueue_style( 'storex-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'storex-bootstrap', get_template_directory_uri() . '/css/bootstrap.css' );
+	wp_enqueue_style( 'storex-font-awesome', get_template_directory_uri() . '/css/font-awesome.css' );
+	wp_enqueue_style( 'storex-header', get_template_directory_uri() . '/css/MyHeader.css' );
+	wp_enqueue_style( 'storex-main', get_template_directory_uri() . '/css/MyMain.css' );
+	wp_enqueue_style( 'storex-footer', get_template_directory_uri() . '/css/MyFooter.css' );
 
 	wp_enqueue_script( 'storex-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'storex-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'storex-bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
