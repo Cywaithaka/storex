cf375d9bd0c7a6d15382ff114a1bc707b8671c77
MrDonald245 <ebocharnikov96@mail.ru>
2015-12-24 11:20:48 +0200

    Добавление шорткодов в footer и functions

--- wp-content/themes/storex/functions.php
+++ wp-content/themes/storex/functions.php
@@ -155,3 +155,30 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+
+/**
+ * Link shortcode handler
+ * example: [link src="img/footer img/visa-icon.png"].
+ *
+ * @param $atts src and href.
+ * @param null $content.
+ * @return string path.
+ */
+function shortcode_link($atts, $content = null)
+{
+	$link = '<!-- link -->';
+	if(isset($atts['src']))
+	{
+		$url = get_template_directory_uri() . '/';
+		$link = ' src="' . $url . $atts['src'] . '" ';
+	}
+	if(isset($atts['href']))
+	{
+		$url = get_template_directory_uri() . '/';
+		$link = ' href="' . $url . $atts['href'] . '" ';
+	}
+	return $link;
+}
+add_shortcode('link', 'shortcode_link');
